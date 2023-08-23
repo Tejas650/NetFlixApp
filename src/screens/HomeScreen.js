2a2a4c921fc91b3d75e0bb8f0174345075186539
Tejas <guptatejas65@gmail.com>
2023-08-23 20:46:25 +0530

    HOMESCREEN WAS DONE. COVERED ALL THINGS IN APP.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Header from '../components/Header';
+import TrendingComponent from '../components/TrendingComponent';
+import MovieRows from '../components/MovieRows';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{flex: 1, backgroundColor: 'black', marginTop: 34}}>
       <Header />
-    </SafeAreaView>
+
+      <TrendingComponent />
+
+      <MovieRows />
+    </ScrollView>
   );
 };
 
